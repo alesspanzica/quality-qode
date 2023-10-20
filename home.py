1aac229e1a1a043b21b637cec1a371cbd48f3a23
Alessia Panzica <61469621+alesspanzica@users.noreply.github.com>
2023-10-20 19:37:37 -0400

    Create home.py

---
+++ home.py
@@ -0,0 +1,7 @@
+"""
+This is implemented for documentation purposes. This class represents the Home page of the web app.
+It would display information, and could guide you to anything within the system. This class would contain
+information and prompts for the backend to be accessed/used. 
+This class for the purpose of Assignment 2 is just a placeholder.
+"""
+print("Welcome to the Homepage!")

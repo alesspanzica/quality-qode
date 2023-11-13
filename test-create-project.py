5aa59ba7a6d7407b5c09e734f8b24f77f667554c
Alessia Panzica <61469621+alesspanzica@users.noreply.github.com>
2023-11-13 18:58:22 -0500

    updated tests

--- test-create-project.py
+++ test-create-project.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 import pytest
-from unittest import TestCase
+import unittest
 import subprocess
 
 from project_manager import ProjectManager
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session
 from database import createSession
 from sqlalchemy.exc import IntegrityError
 
-class CreateProjects:
+class CreateProjectsTasks:
     def setup_class(self):
         self.session = createSession()
         self.valid_project = ProjectModel(
@@ -22,9 +22,18 @@ class CreateProjects:
             owner = "Samuel Patel",
             num_of_tasks = 0
         )
+        self.valid_task = TaskModel(
+            task_name = "Test Task",
+            assigned_project = 1234,
+            priority = "Low",
+            capability_level = 1,
+            deadline = "01-01-01",
+            task_num_in_project = 1,
+            project_portion = 0.0
+        )
 
-    def test_project_valid(self):   
-        self.session.add(self.valid_author)
+    def test_db_project_valid(self):   
+        self.session.add(self.valid_project)
         self.session.commit()
         test_proj_query = self.session.query(ProjectModel).filter_by(project_id=1234).first()
         assert test_proj_query.project_name == "Test Project"
@@ -33,9 +42,21 @@ class CreateProjects:
         assert test_proj_query.priority == "Low"
         assert test_proj_query.owner == "Samuel Patel"
         assert test_proj_query.num_of_tasks == 0
+    
+    def test_db_task_valid(self):   
+        self.session.add(self.valid_task)
+        self.session.commit()
+        test_task_query = self.session.query(TaskModel).filter_by(task_num_in_project=1, assigned_project=1234).first()
+        assert test_task_query.project_name == "Test Task"
+        assert test_task_query.assigned_project != 1235
+        assert test_task_query.priority == "Low"
+        assert test_task_query.capability_level == 1
+        assert test_task_query.deadline == "01-01-01"
+        assert test_task_query.num_of_tasks != 0
+        assert test_task_query.project_portion == 0.0
 
     @pytest.mark.xfail(raises=IntegrityError)
-    def test_project_missing_field(self):
+    def test_db_project_missing_field(self):
         testing = ProjectModel(
             project_name = "Test Project 1",
             project_id = 2345,
@@ -48,6 +69,24 @@ class CreateProjects:
             self.session.commit()
         except IntegrityError:
             self.session.rollback()
+    
+    @pytest.mark.xfail(raises=IntegrityError)
+    def test_db_task_missing_field(self):
+        testing = TaskModel(
+            project_name = "Test Project 1",
+            project_id = 2345,
+            deadline = "10-10-10",
+            priority = "Low",
+            num_of_tasks = 0
+        )
+        self.session.add(testing)
+        try:
+            self.session.commit()
+        except IntegrityError:
+            self.session.rollback()
+    
+   # def test_func_project_valid(self):
+
 
     def teardown_class(self):
         self.session.rollback()

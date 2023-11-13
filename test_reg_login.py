29871131c284eca34449bfbbe7da8d5baab99af6
JasperCLim <132617110+JasperCLim@users.noreply.github.com>
2023-11-13 18:25:03 -0500

    working test scripts for registration
    plus minor reworks for registration:
    1. completion of registration now calls main instead of login
    2. main has an exit option

--- test_reg_login.py
+++ test_reg_login.py
@@ -3,19 +3,28 @@ import pytest
 from unittest import TestCase
 from registration import Registration
 from model import User as UserModel
+from sqlalchemy import delete
 from user import User
 from sqlalchemy.orm import Session
 from database import createSession
 
 #Script to test if a user can register and is added to the database.
 def test_register(monkeypatch, capsys):
-        responses = iter(['testuser', 'testpass', 'testname', 'testnumber', 'testemail', 0,
-                          0])
+        session = createSession()
+        firsttime = session.query(UserModel).filter(UserModel.username == "testpass")
+        if firsttime:
+            stmt = delete(UserModel).where(UserModel.username == "testpass")
+        session.close()
+        responses = iter(['testuser', 'testpass', 'testname', 'testnumber', 'testemail', 
+                          '0', "test2","0"])
         monkeypatch.setattr('builtins.input', lambda msg: next(responses))
         Registration.register()
         captured = capsys.readouterr()
-        cap = "Registration successful! Please log in.\n"
-        assert captured.out == "Registration successful! Please log in.\n"
+        captured2 = []
+        for i in range(40,0, -1):
+            captured2 += captured.out[-i]
+        captured2 = ''.join(captured2)
+        assert captured2 == "Registration successful! Please log in.\n"
 
 #Script to test if user can successfully login.
 def test_login():
